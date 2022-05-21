1a35bf2f6a20d2086cc80f8ac95adf515b95fcdb
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-05-20 20:48:07 -0600

    input pushes geocode

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var apiKey = "29677fe20199fe6d91bf4ac066217d18";
-var city = "denver";
+var city = localStorage.getItem("city");
 var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 var citySearch = $("#search-focus");
 var searchBtn = $("#searchBtn");
@@ -12,7 +12,7 @@ var weatherSetting = {
   url: "",
   method: "GET",
 };
-
+console.log(city);
 // creat function to get current weather conditions
 
 // create function to get five day forcast
@@ -28,6 +28,10 @@ function coordinates(geoUrl) {
       var lon = data[0].lon;
       console.log(lat);
       console.log(lon);
+
+      var name = data[0].name;
+      console.log(name);
+
       // call get weather function passing lat and lon as para
       var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=imperial&appid=${apiKey}`;
 
@@ -48,7 +52,6 @@ coordinates(geoUrl);
 
 function searchApi() {
   city = citySearch.val();
-  localStorage.setItem("city", city);
   console.log(city.val);
   weatherSetting.url = console.log(weatherSetting.url);
 
@@ -58,20 +61,21 @@ function searchApi() {
 searchBtn.on("click", function (event) {
   event.preventDefault();
   var userInput = citySearch.val().toLowerCase();
+
   localStorage.setItem("city", userInput);
   // add citys to list being created
 
   function createCityList() {
-    var unordLi = $("<ul>");
+    var unordLi = $("<div>");
+    unordLi.addClass("col-2 border-light bg-danger");
     cityList.append(unordLi);
     unordLi.attr("id", "list");
     $("#list").text(userInput);
 
-    // need for loop to pull from array of five day forcast
-
-    var cities = localStorage.getItem("city");
     $("list").append(unordLi);
-    unordLi.text(cities);
+    unordLi.text(localStorage.getItem("city"));
   }
   createCityList();
+  searchApi();
 });
+// need for loop to pull from array of five day forcast
