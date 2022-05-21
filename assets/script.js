950c0f90d6b9cd12410df91fefc0c0c352edf85d
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-05-20 21:31:13 -0600

    userInput pulls up the long and lat

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,25 @@
 var apiKey = "29677fe20199fe6d91bf4ac066217d18";
-var city = localStorage.getItem("city");
-var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+var city = "";
+
 var citySearch = $("#search-focus");
 var searchBtn = $("#searchBtn");
 var cityList = $(".cityList");
 var currentSrch = "";
-
+console.log(city);
 var weatherSetting = {
   async: true,
   crossDomain: true,
   url: "",
   method: "GET",
 };
-console.log(city);
+
 // creat function to get current weather conditions
 
 // create function to get five day forcast
 
-function coordinates(geoUrl) {
+function coordinates() {
+  var geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+
   fetch(geoUrl)
     .then(function (response) {
       return response.json();
@@ -30,6 +32,7 @@ function coordinates(geoUrl) {
       console.log(lon);
 
       var name = data[0].name;
+
       console.log(name);
 
       // call get weather function passing lat and lon as para
@@ -48,7 +51,7 @@ function coordinates(geoUrl) {
     });
 }
 
-coordinates(geoUrl);
+// coordinates(geoUrl);
 
 function searchApi() {
   city = citySearch.val();
@@ -60,22 +63,24 @@ function searchApi() {
 
 searchBtn.on("click", function (event) {
   event.preventDefault();
-  var userInput = citySearch.val().toLowerCase();
-
-  localStorage.setItem("city", userInput);
+  city = citySearch.val().toLowerCase();
+  console.log(city);
+  localStorage.setItem("city", city);
   // add citys to list being created
-
-  function createCityList() {
-    var unordLi = $("<div>");
-    unordLi.addClass("col-2 border-light bg-danger");
-    cityList.append(unordLi);
-    unordLi.attr("id", "list");
-    $("#list").text(userInput);
-
-    $("list").append(unordLi);
-    unordLi.text(localStorage.getItem("city"));
-  }
+  coordinates();
   createCityList();
-  searchApi();
 });
+function createCityList() {
+  var unordLi = $("<div>");
+  unordLi.addClass("col-2 border-light bg-danger");
+  cityList.append(unordLi);
+  unordLi.attr("id", "list");
+  $("#list").text(city);
+
+  $("list").append(unordLi);
+  unordLi.text(localStorage.getItem("city"));
+}
+
+// searchApi();
+
 // need for loop to pull from array of five day forcast
